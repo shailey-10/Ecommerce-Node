ad309fa22fe9e60d07335633ddd98aa4a47a526b
shailey-10 <35800970+shailey-10@users.noreply.github.com>
2022-04-16 19:21:25 +0530

    First Commit - Addig Local Project To Github

---
+++ util/database.js
@@ -0,0 +1,28 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db; 
+
+const mongoConnect = (callback) => {
+
+MongoClient.connect('mongodb://Shailey:3Ym26f8JYuvcPUDE@cluster0-bopos.mongodb.net/shop?retryWrites=true&w=majority')
+.then(client => {
+    console.log('CONNECTED!');
+    _db = client.db();
+    callback();
+})
+.catch(err => { 
+    console.log(err);
+    throw err;
+});
+};
+
+const getDb = () => {
+    if(_db){
+        return _db;
+    }
+    throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb =getDb;
